686dc6005e5bf9efab33edc554b9478dd5cde4b1
Christopher Walden <checkdoesgames@gmail.com>
2025-11-05 09:29:33 -0800

    Removed footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import "./Home.module.css";
 import Navbar from "../components/Navbar.jsx";
 import MainContent from "../components/MainContent.jsx";
-import Footer from "../components/Footer.jsx";
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <MainContent />
-            <Footer />
         </>
     );
 }
