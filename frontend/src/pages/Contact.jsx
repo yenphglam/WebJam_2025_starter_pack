10f5f216f779ced3f935cccb7443257ecc051617
Allison Hua <huaat@uci.edu>
2025-11-05 18:07:32 -0800

    Added to Contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,4 +1,21 @@
 import styles from "./Contact.module.css";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+function createData(name, email) {
+    return { name, email };
+}
+
+const rows = [
+    createData('Allison Hua', 'huaat@uci.edu'),
+    createData('Christine Nguyen', 'chrisln4@uci.edu'),
+    createData('Christopher Walden', 'checkdoesgames@gmail.com'),
+    createData('Yen Phuong Lam', 'lamyp@uci.edu'),
+];
 
 export default function Contact() {
     return (
@@ -6,9 +23,28 @@ export default function Contact() {
             <div className={styles.headerSection}>
                 <h1>Contact</h1>
             </div>
-            <div className={styles.contactSection}>
-                wrgh
-            </div>
+            <TableContainer className={styles.contactTableContainer}>
+                <Table className={styles.contactTable} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><strong>Name</strong></TableCell>
+                            <TableCell><strong>Email</strong></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     );
 }
